55922e27ab4491acb6d3bdd3044197662950043a
Sean <sean02050923@gmail.com>
2026-01-21 13:31:14 +0800

    chore : 新增vite-plugin-mock、mockjs套件並配置 vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import { viteMockServe } from "vite-plugin-mock";
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    viteMockServe({
+      mockPath: "mock", // 指定 mock 檔案存放路徑
+      localEnabled: true, // 開發環境下開啟
+    }),
+  ],
   server: {
     host: "0.0.0.0", // 允許外部存取容器內的 Vite 服務
     port: 5173, // 確保與 compose.yml 的 port 一致
