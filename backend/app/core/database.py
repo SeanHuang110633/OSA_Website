b9bb29a95c1ae31b03e5e04adb789ce8abf6d9a9
Sean <sean02050923@gmail.com>
2026-01-22 17:59:37 +0800

    fix : aiven已經停用，故刪除與aiven相關的部分，並修改部分註解讓其清晰

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,38 +1,31 @@
+# backend/app/core/database.py
 import os, ssl
 from typing import Generator
 from sqlmodel import create_engine, SQLModel, Session
 from dotenv import load_dotenv
 
 
-# 1. 載入環境變數
+# 1. 讀取專案根目錄下的 .env 檔案，並將其中的鍵值對注入到系統的環境變數中
 load_dotenv()
 
-# 2. 取得連線字串
-# 改回純粹從環境變數讀取，不留 SQLite 預設值
+# 2. 取得連線字串(從環境變數讀取)
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # 2.1 確保 DATABASE_URL 存在
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL not found in .env file or environment variables")
 
-# 這些只是為了aiven，後續上線不用管它們
-# --- (核心修改) 建立自定義 SSL Context ---
-# 目的：允許加密連線，但跳過對 Aiven 自簽名憑證的驗證
-ssl_context = ssl.create_default_context()
-ssl_context.check_hostname = False
-ssl_context.verify_mode = ssl.CERT_NONE    
-
+   
 # 3. 建立 Engine
+# Engine 是 SQLAlchemy/SQLModel 與資料庫對話的「工廠」，它持有方言（Dialect，這裡是 MySQL）的處理邏輯與連線池管理
 # pool_recycle=3600: 每小時自動回收連線，防止 MySQL 閒置過久斷線
-# pool_pre_ping=True: 針對 Aiven 雲端資料庫建議開啟，連線前會先測試有效性
+# pool_pre_ping=True: 在應用程式每次使用連線前，會先發送一個類似 SELECT 1 的微小指令給資料庫，確認連線是否還活著(我們的環境應該不需要但可以先留著)
 # echo=True: 開發時顯示 SQL，上線部署時可改為 False
 engine = create_engine(
     DATABASE_URL, 
     echo=True, 
     pool_recycle=3600,
     pool_pre_ping=True,
-    # 將 ssl_context 傳入 connect_args
-    connect_args={"ssl": ssl_context}
 )
 
 # 4. 建立資料庫和表格
