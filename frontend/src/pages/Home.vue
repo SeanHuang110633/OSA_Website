c2002ec2965b4d30b0ef3d80b220c82ee64665d9
KO,YUNG-ching <cordeliake@gmail.com>
2026-01-27 00:18:08 +0900

    Home

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="container">
+    <HeroBanner class="section" />
+    <NewsAndLinks class="section" />
+    <EventsRow class="section" />
+  </main>
+
+  <QuickLinksGrid class="section" />
+  <main class="container">
+    <MapSection class="section" />
+  </main>
+</template>
+
+<script setup>
+import HeroBanner from "../components/HeroBanner.vue";
+import NewsAndLinks from "../components/NewsAndLinks.vue";
+import EventsRow from "../components/EventsRow.vue";
+import QuickLinksGrid from "../components/QuickLinksGrid.vue";
+import MapSection from "../components/MapSection.vue";
+</script>
