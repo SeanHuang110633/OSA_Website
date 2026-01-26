31d0c4f285546ae33af196b58e86cdde3796c5a6
KO,YUNG-ching <cordeliake@gmail.com>
2026-01-27 00:44:40 +0900

    套件沒載到，已調整好

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+
 import Home from "../pages/Home.vue";
 import NewsList from "../pages/NewsList.vue";
 import ServiceResources from "../pages/ServiceResources.vue";
@@ -8,22 +9,16 @@ import AboutUs from "../pages/AboutUs.vue";
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", component: Home },
-    { path: "/news", component: NewsList },
-    { path: "/resources", component: ServiceResources },
+    { path: "/", name: "home", component: Home },
+    { path: "/news", name: "news", component: NewsList },
+    { path: "/resources", name: "resources", component: ServiceResources },
     { path: "/downloads", name: "downloads", component: Downloads },
     { path: "/about", name: "about", component: AboutUs },
   ],
-
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) return savedPosition;
-
     if (to.hash) {
-      return {
-        el: to.hash,
-        behavior: "smooth",
-        top: 90, 
-      };
+      return { el: to.hash, behavior: "smooth", top: 90 };
     }
     return { top: 0 };
   },
