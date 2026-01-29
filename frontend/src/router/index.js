f3f19999b11562424425d9ce7d2a6f91fb30e547
155Julia <julia921116@gmail.com>
2026-01-29 16:10:29 +0800

    feat: 新增募款專區頁面及相關圖片、component

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import OriginalExamples from '../pages/originalExamples.vue'
+import Donate from '../pages/donationPage.vue'
 
 const routes = [
   { path: '/example', name: 'OriginalExamples', component: OriginalExamples },
+  { path: '/donate', name: 'Donate', component: Donate },
   { path: '/', redirect: '/example' },
 ]
 
