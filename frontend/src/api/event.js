6f2c9b91348360ed831d0a62298e5e23486de8d7
Sean <sean02050923@gmail.com>
2026-01-30 14:48:46 +0800

    feat : 完成API串接與查看更多轉跳

--- frontend/src/api/event.js
+++ frontend/src/api/event.js
@@ -1,6 +1,6 @@
 import request from "./request";
 
-// 取得活動列表
+// 取得消息列表
 export function getEvents(params) {
   // params 包含: page, size, locale
   return request({
@@ -10,7 +10,7 @@ export function getEvents(params) {
   });
 }
 
-// 取得單一活動詳情
+// 取得單一消息詳情
 export function getEventDetail(id, locale) {
   return request({
     url: `/events/${id}`,
