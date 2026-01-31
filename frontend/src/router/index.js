bf930503573dbf85bf804a3a864cf8e4e96ac5b4
Sean <sean02050923@gmail.com>
2026-01-31 18:42:41 +0800

    feat : 手動調整tidyUp和donatingPage的更新

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,7 @@
-
-import { createRouter, createWebHistory } from "vue-router";
-import HomePage from "../pages/HomePage.vue";
-import NewsListPage from "../pages/NewsListPage.vue";
-import ServiceResourcesPage from "../pages/ServiceResourcesPage.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from "../pages/HomePage.vue";
+import NewsList from "../pages/NewsList.vue";
+import ServiceResources from "../pages/ServiceResources.vue";
 // import Downloads from "../pages/Downloads.vue"; 先改成下面這個
 import DownloadPage from "../pages/DownloadPage.vue";
 import AboutUsPage from "../pages/AboutUsPage.vue";
@@ -12,10 +11,11 @@ import DonationPage from "../pages/DonationPage.vue";
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", name: "home", component: HomePage },
-    { path: "/news", name: "news", component: NewsListPage },
+    { path: "/", name: "home", component: Home },
+    { path: "/news", name: "news", component: NewsList },
     { path: "/events/:id", name: "event-detail", component: EventDetailPage },
-    { path: "/resources", name: "resources", component: ServiceResourcesPage },
+    { path: "/resources", name: "resources", component: ServiceResources },
+    // { path: "/downloads", name: "downloads", component: Downloads }, 先改成下面這個
     { path: "/downloads", name: "downloads", component: DownloadPage },
     { path: "/about", name: "about", component: AboutUsPage },
     { path: "/donate", name: "donate", component: DonationPage },
